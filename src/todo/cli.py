4274aabaf6fe292568ad3cc4efce9f390a51d8ba
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-02-01 00:35:30 +0100

    feat: implement CLI command for creating tasks
    Refs #28

--- src/todo/cli.py
+++ src/todo/cli.py
@@ -9,3 +9,9 @@ def cli() -> None:
 @cli.command()
 def list_tasks() -> None:
     click.echo("There are no tasks currently.")
+
+
+@cli.command()
+@click.argument("description")
+def create_task(description: str) -> None:
+    click.echo("Task added successfully.")
