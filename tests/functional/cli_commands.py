16e27ae2eb362d6a34f372c847b8cfb39eb81d9a
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-02-09 15:11:41 +0100

    test: add acceptance test for US: Change task status
    Closes #33

--- tests/functional/cli_commands.py
+++ tests/functional/cli_commands.py
@@ -9,3 +9,7 @@ def create_task(runner: testing.CliRunner, description: str) -> testing.Result:
 
 def list_tasks(runner: testing.CliRunner) -> testing.Result:
     return runner.invoke(cli.cli, ["list-tasks"])
+
+
+def change_task_status(runner: testing.CliRunner, task_id: str) -> testing.Result:
+    return runner.invoke(cli.cli, ["change-task-status", task_id])
