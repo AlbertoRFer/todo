4bb4a27e675624d6ee7de6b50c42ba36620c1b9b
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-02-11 21:35:21 +0100

    fix(repository): update bootstrap

--- src/todo/bootstrap.py
+++ src/todo/bootstrap.py
@@ -1,10 +1,9 @@
 import pathlib
 
-from todo import repository, storage, todo_app
+from todo import repository, todo_app
 
 
 def create_todo_app() -> todo_app.TodoApp:
     storage_path = pathlib.Path.cwd() / "todo.json"
-    repo_storage = storage.JsonStorage(storage_path)
-    repo = repository.TaskRepository(repo_storage)
+    repo = repository.TodoListJsonRepository(storage_path)
     return todo_app.TodoApp(repo)
