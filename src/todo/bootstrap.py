047eaef2ad6408a4ae732b3917e4514adcd0891c
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-02-11 23:16:36 +0100

    feat: update bootstrap  with repo initialization

--- src/todo/bootstrap.py
+++ src/todo/bootstrap.py
@@ -6,4 +6,5 @@ from todo import repository, todo_app
 def create_todo_app() -> todo_app.TodoApp:
     storage_path = pathlib.Path.cwd() / "todo.json"
     repo = repository.TodoListJsonRepository(storage_path)
+    repo.initialize()
     return todo_app.TodoApp(repo)
