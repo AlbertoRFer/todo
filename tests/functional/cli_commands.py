e480318fe803afa97e42df8750b72bd3387a4d47
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2026-02-13 14:41:15 +0100

    test: improve cli command calls in the tests
    - disable Click exception handling for tests to show more information when they fail
    - reduce duplication in command calling

--- tests/functional/cli_commands.py
+++ tests/functional/cli_commands.py
@@ -3,13 +3,23 @@ from click import testing
 from todo import cli
 
 
+def invoke_runner(runner: testing.CliRunner, cmd: str, *args: str) -> testing.Result:
+    return runner.invoke(cli.cli, [cmd, *args], standalone_mode=False)
+
+
 def create_task(runner: testing.CliRunner, description: str) -> testing.Result:
-    return runner.invoke(cli.cli, ["create-task", description])
+    cmd = "create-task"
+
+    return invoke_runner(runner, cmd, description)
 
 
 def list_tasks(runner: testing.CliRunner) -> testing.Result:
-    return runner.invoke(cli.cli, ["list-tasks"])
+    cmd = "list-tasks"
+
+    return invoke_runner(runner, cmd)
 
 
 def change_task_status(runner: testing.CliRunner, task_id: str) -> testing.Result:
-    return runner.invoke(cli.cli, ["change-task-status", task_id])
+    cmd = "change-task-status"
+
+    return invoke_runner(runner, cmd, task_id)
--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -4,4 +4,4 @@ from click import testing
 
 @pytest.fixture
 def runner() -> testing.CliRunner:
-    return testing.CliRunner()
+    return testing.CliRunner(catch_exceptions=False)
